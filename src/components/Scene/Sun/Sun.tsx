1ef72f983479192a55beca54fd48d8a3682f2758
Tim Simms <tim-simms@hotmail.com>
2025-03-24 11:23:29 +0000

    Sun Reflection

--- src/components/Scene/Sun/Sun.tsx
+++ src/components/Scene/Sun/Sun.tsx
@@ -1,10 +1,13 @@
 import * as THREE from "three";
 import sun from "../../../assets/sun.png";
+import sunBlur from "../../../assets/sunBlur.png";
 
 export default function Sun() {
   const planeGeometry = new THREE.PlaneGeometry(20, 20);
   const textureLoader = new THREE.TextureLoader();
   const projectsTexture = textureLoader.load(sun);
+  const blurTexture = textureLoader.load(sunBlur);
+  blurTexture.colorSpace = THREE.SRGBColorSpace;
   projectsTexture.colorSpace = THREE.SRGBColorSpace;
 
   const humanMaterial = new THREE.MeshBasicMaterial({
@@ -12,6 +15,13 @@ export default function Sun() {
     transparent: true,
     opacity: 1,
     depthWrite: false,
+  });
+  const blurMaterial = new THREE.MeshStandardMaterial({
+    map: blurTexture,
+    transparent: true,
+    opacity: 1,
+    depthWrite: false,
+    roughness: 0,
   });
 
   return (
@@ -22,6 +32,13 @@ export default function Sun() {
         rotation={[-Math.PI * 0, 0, 0]}
         position={[-30, -44.5, -150]}
         scale={7}
+      />
+      <mesh
+        geometry={planeGeometry}
+        material={blurMaterial}
+        rotation={[-Math.PI * 0.5, 0, Math.PI * 0.032]}
+        position={[-8, -9.2, -36]}
+        scale={2.3}
       />
     </>
   );
